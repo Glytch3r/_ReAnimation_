84e4abaf6740e750e7e95418b079fd2cb43c9480
Pao <picchetti1998@gmail.com>
2023-02-12 23:51:40 +0100

    Formatted most files

--- media/lua/client/undeadMenu1.lua
+++ media/lua/client/undeadMenu1.lua
@@ -1,6 +1,3 @@
-
-
-
 --[[⠀
 ----------------------------------------------------------------------------------------------------------------------------⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                       ⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⣰⣦⣀⣀⠀⠠⠄⠄⠠⠀⠀⢀⣠⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -23,23 +20,22 @@ Discord: Glytch3r#2892
 
 ----------------------------------------------------------------------------------------------------------------------------
 --]]
-
---if not (getCore():getDebug() or isAdmin()) then return; end 
+--if not (getCore():getDebug() or isAdmin()) then return; end
 
 
 ReanimationMenu = ISCollapsableWindow:derive("ReanimationMenu");
 
 ReanimationMenu.cheat = true
 ReanimationMenu.instance = nil
-        local xOrigin = getCore():getScreenWidth()-518
-        local yOrigin = getCore():getScreenHeight()-440
+local xOrigin = getCore():getScreenWidth() - 518
+local yOrigin = getCore():getScreenHeight() - 440
 function ReanimationMenu.openPanel(playerObj)
     if ReanimationMenu.instance == nil then
-        local window = ReanimationMenu:new(xOrigin, yOrigin, 125-12, 270, playerObj)
+        local window = ReanimationMenu:new(xOrigin, yOrigin, 125 - 12, 270, playerObj)
         window:initialise()
         window:addToUIManager()
-      
-    end   ReanimationMenu.instance = window
+    end
+    ReanimationMenu.instance = window
 end
 
 ------------------------               ---------------------------
@@ -49,151 +45,164 @@ end
 --**************************            	   **************************
 
 
-	local opTitle1 = "SetUndead"
-	local opTitle2 = "Scream!"
-	local opTitle3 = "recovery"
-	local opTitle4 = "viral"
-	local opTitle5 = "zedblaze"
-    local opTitle6 = "zeddeath"
-	local opTitle7 = "injury"
+local opTitle1 = "SetUndead"
+local opTitle2 = "Scream!"
+local opTitle3 = "recovery"
+local opTitle4 = "viral"
+local opTitle5 = "zedblaze"
+local opTitle6 = "zeddeath"
+local opTitle7 = "injury"
 
 --**************************            	   **************************
 --								 body***
 --**************************            	   **************************
 function ReanimationMenu:onClick(button)
+    if button.internal == opTitle1 then
+        print(opTitle1);
+        --start----------------------               ---------------------------start
 
-    if button.internal == opTitle1 then print(opTitle1); 
---start----------------------               ---------------------------start
-
-  if  not  getPlayer()  then return end 
-local pl = getPlayer() 
-local inv = pl:getInventory() 
-pl:clearWornItems();
-inv:clear();
-pl:resetModel();
-local item = "Skin.Bones"
-local equip = inv:AddItem(item);
-equip:getVisual():setTextureChoice(3);
-pl:setWornItem(equip:getBodyLocation(), equip);
-if pl:getModData()['glytchprint'] then 
-    pl:getModData()['glytchprint'] = nil
-    else
-    pl:getModData()['glytchprint'] = true; 
-end
-
---getPlayer():setVariable('isUndead', 'true')	
---end----------------------               ---------------------------end
-	elseif button.internal == opTitle2 then print(opTitle2); 
---start----------------------            ---------------------------start
-local player = getPlayer()
-glytchrage()
-player:getBodyDamage():RestoreToFullHealth()
-player:getZombieKills()
---Events.OnTick.Add() 
-
-
---end----------------------            ---------------------------end
-    elseif button.internal == opTitle3 then print(opTitle3); 
---start----------------------           ---------------------------start
-
-    if getPlayer():getModData()['undeadheal'] then 
-        getPlayer():getModData()['undeadheal'] = false
-        Events.OnPlayerUpdate.Remove(undeadRecovery) 
+        if not getPlayer() then return end
+        local pl = getPlayer()
+        local inv = pl:getInventory()
+        pl:clearWornItems();
+        inv:clear();
+        pl:resetModel();
+        local item = "Skin.Bones"
+        local equip = inv:AddItem(item);
+        equip:getVisual():setTextureChoice(3);
+        pl:setWornItem(equip:getBodyLocation(), equip);
+        if pl:getModData()['glytchprint'] then
+            pl:getModData()['glytchprint'] = nil
         else
-        getPlayer():getModData()['undeadheal'] = true; 
-        Events.OnPlayerUpdate.Add(undeadRecovery) 
-    end
+            pl:getModData()['glytchprint'] = true;
+        end
+
+        --getPlayer():setVariable('isUndead', 'true')	
+        --end----------------------               ---------------------------end
+    elseif button.internal == opTitle2 then
+        print(opTitle2);
+        --start----------------------            ---------------------------start
+        local player = getPlayer()
+        glytchrage()
+        player:getBodyDamage():RestoreToFullHealth()
+        player:getZombieKills()
+        --Events.OnTick.Add()
 
 
---end----------------------            ---------------------------end
-    elseif button.internal == opTitle4 then print(opTitle4); 
---start----------------------         ---------------------------start
-    if getPlayer():getModData()['glytchviral'] then 
-        getPlayer():getModData()['glytchviral'] = nil
+        --end----------------------            ---------------------------end
+    elseif button.internal == opTitle3 then
+        print(opTitle3);
+        --start----------------------           ---------------------------start
+
+        if getPlayer():getModData()['undeadheal'] then
+            getPlayer():getModData()['undeadheal'] = false
+            Events.OnPlayerUpdate.Remove(undeadRecovery)
         else
-        getPlayer():getModData()['glytchviral'] = true; 
-    end
---end----------------------            -----------local ----------------end
-    elseif button.internal == opTitle5 then print(opTitle5); 
---start----------------------         ---------------------------start
-    if getPlayer():getModData()['zedblaze'] then 
-        getPlayer():getModData()['zedblaze'] = nil
+            getPlayer():getModData()['undeadheal'] = true;
+            Events.OnPlayerUpdate.Add(undeadRecovery)
+        end
+
+
+        --end----------------------            ---------------------------end
+    elseif button.internal == opTitle4 then
+        print(opTitle4);
+        --start----------------------         ---------------------------start
+        if getPlayer():getModData()['glytchviral'] then
+            getPlayer():getModData()['glytchviral'] = nil
         else
-        getPlayer():getModData()['zedblaze'] = true; 
-    end
---end----------------------            ---------------------------end
-    elseif button.internal == opTitle6 then print(opTitle6);
---start----------------------         ---------------------------start
-    if getPlayer():getModData()['zeddeath'] then 
-        getPlayer():getModData()['zeddeath'] = nil
+            getPlayer():getModData()['glytchviral'] = true;
+        end
+        --end----------------------            -----------local ----------------end
+    elseif button.internal == opTitle5 then
+        print(opTitle5);
+        --start----------------------         ---------------------------start
+        if getPlayer():getModData()['zedblaze'] then
+            getPlayer():getModData()['zedblaze'] = nil
         else
-        getPlayer():getModData()['zeddeath'] = true; 
-    end
---end----------------------            ---------------------------end
-    elseif button.internal == opTitle7 then print(opTitle7);
---start----------------------         ---------------------------start
-glytchinjury()
---[[     if getPlayer():getModData()['zedstag'] then 
+            getPlayer():getModData()['zedblaze'] = true;
+        end
+        --end----------------------            ---------------------------end
+    elseif button.internal == opTitle6 then
+        print(opTitle6);
+        --start----------------------         ---------------------------start
+        if getPlayer():getModData()['zeddeath'] then
+            getPlayer():getModData()['zeddeath'] = nil
+        else
+            getPlayer():getModData()['zeddeath'] = true;
+        end
+        --end----------------------            ---------------------------end
+    elseif button.internal == opTitle7 then
+        print(opTitle7);
+        --start----------------------         ---------------------------start
+        glytchinjury()
+        --[[     if getPlayer():getModData()['zedstag'] then
         getPlayer():getModData()['zedstag'] = nil
         else
-        getPlayer():getModData()['zedstag'] = true; 
+        getPlayer():getModData()['zedstag'] = true;
     end ]]
---end----------------------            ---------------------------end
-	end
+        --end----------------------            ---------------------------end
+    end
 end
-    
+
 function ReanimationMenu:createChildren()
     ISCollapsableWindow.createChildren(self);
 
     local th = self:titleBarHeight()
-    local buttonWid = 90-12
+    local buttonWid = 90 - 12
     local buttonHgt = 24
 
 
 
-    self.opt1 = ISButton:new(self:getWidth()/2 - buttonWid/2, th + 10, buttonWid, buttonHgt, opTitle1, self, ReanimationMenu.onClick);
+    self.opt1 = ISButton:new(self:getWidth() / 2 - buttonWid / 2, th + 10, buttonWid, buttonHgt, opTitle1, self,
+            ReanimationMenu.onClick);
     self.opt1.internal = opTitle1;
     self.opt1:initialise();
     self.opt1:instantiate();
     self:addChild(self.opt1);
 
-    self.opt2 = ISButton:new(self:getWidth()/2 - buttonWid/2, self.opt1:getBottom() + 10, buttonWid, buttonHgt, opTitle2, self, ReanimationMenu.onClick);
+    self.opt2 = ISButton:new(self:getWidth() / 2 - buttonWid / 2, self.opt1:getBottom() + 10, buttonWid, buttonHgt,
+            opTitle2, self, ReanimationMenu.onClick);
     self.opt2.internal = opTitle2;
     self.opt2:initialise();
     self.opt2:instantiate();
     self:addChild(self.opt2);
 
-    self.opt3 = ISButton:new(self:getWidth()/2 - buttonWid/2, self.opt2:getBottom() + 10, buttonWid, buttonHgt, opTitle3, self, ReanimationMenu.onClick);
+    self.opt3 = ISButton:new(self:getWidth() / 2 - buttonWid / 2, self.opt2:getBottom() + 10, buttonWid, buttonHgt,
+            opTitle3, self, ReanimationMenu.onClick);
     self.opt3.internal = opTitle3;
     self.opt3:initialise();
     self.opt3:instantiate();
     self:addChild(self.opt3);
-	
-	self.opt4 = ISButton:new(self:getWidth()/2 - buttonWid/2+6, self.opt3:getBottom() + 10, buttonWid-12, buttonHgt, opTitle4, self, ReanimationMenu.onClick);
+
+    self.opt4 = ISButton:new(self:getWidth() / 2 - buttonWid / 2 + 6, self.opt3:getBottom() + 10, buttonWid - 12,
+            buttonHgt, opTitle4, self, ReanimationMenu.onClick);
     self.opt4.internal = opTitle4;
     self.opt4:initialise();
     self.opt4:instantiate();
     self:addChild(self.opt4);
-    
-    self.opt5 = ISButton:new(self:getWidth()/2 - buttonWid/2, self.opt4:getBottom() + 10, buttonWid, buttonHgt, opTitle5, self, ReanimationMenu.onClick);
+
+    self.opt5 = ISButton:new(self:getWidth() / 2 - buttonWid / 2, self.opt4:getBottom() + 10, buttonWid, buttonHgt,
+            opTitle5, self, ReanimationMenu.onClick);
     self.opt5.internal = opTitle5;
     self.opt5:initialise();
     self.opt5:instantiate();
     self:addChild(self.opt5);
-    
-    self.opt6 = ISButton:new(self:getWidth()/2 - buttonWid/2, self.opt5:getBottom() + 10, buttonWid, buttonHgt, opTitle6, self, ReanimationMenu.onClick);
+
+    self.opt6 = ISButton:new(self:getWidth() / 2 - buttonWid / 2, self.opt5:getBottom() + 10, buttonWid, buttonHgt,
+            opTitle6, self, ReanimationMenu.onClick);
     self.opt6.internal = opTitle6;
     self.opt6:initialise();
     self.opt6:instantiate();
     self:addChild(self.opt6);
 
-    self.opt7 = ISButton:new(self:getWidth()/2 - buttonWid/2, self.opt6:getBottom() + 10, buttonWid, buttonHgt, opTitle7, self, ReanimationMenu.onClick);
+    self.opt7 = ISButton:new(self:getWidth() / 2 - buttonWid / 2, self.opt6:getBottom() + 10, buttonWid, buttonHgt,
+            opTitle7, self, ReanimationMenu.onClick);
     self.opt7.internal = opTitle7;
     self.opt7:initialise();
     self.opt7:instantiate();
     self:addChild(self.opt7);
-	
 end
+
 --**************************            	   **************************
 function ReanimationMenu:close()
     ReanimationMenu.instance = nil
@@ -211,27 +220,24 @@ function ReanimationMenu:new(x, y, width, height, character)
 end
 
 function ReanimationMenu.press(key)
-    if (key==207) then 
-        if not (getCore():getDebug() or isAdmin()) then return; end   
+    if (key == 207) then
+        if not (getCore():getDebug() or isAdmin()) then return; end
         ReanimationMenu.openPanel()
         return key
     end
-	if  (key == getCore():getKey("Emote") or key == getCore():getKey("Shout")) then
-        if not (getCore():getDebug() or isAdmin()) then return; end   
-        if getPlayer():getModData()['glytchprint'] then end
+    if (key == getCore():getKey("Emote") or key == getCore():getKey("Shout")) then
+        if not (getCore():getDebug() or isAdmin()) then return; end
+        if getPlayer():getModData()['glytchprint'] then
+        end
 
 
         -- TODO Move this
 
 
-        getPlayer():playEmote("isZedScream") 
+        getPlayer():playEmote("isZedScream")
         glytchrage()
         return key
     end
-
-
 end
 
-
 Events.OnKeyPressed.Add(ReanimationMenu.press);
-
