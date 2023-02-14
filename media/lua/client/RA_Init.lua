231921b42d9801398ede73db7061236e8a0b0d81
Pao <picchetti1998@gmail.com>
2023-02-14 18:53:22 +0100

    Reformat some stuff, test with player visuals

--- media/lua/client/RA_Init.lua
+++ media/lua/client/RA_Init.lua
@@ -14,8 +14,7 @@ local function InitZedRegen(player)
     -- TODO Copy player data and save it somewhere
     -- TODO Check if infected and if we can show the "Respawn as Zombie" button 
 
-    
-end
 
+end
 
 Events.OnPlayerDeath.Add(InitZedRegen)
--- media/lua/client/RA_PostDeathMenu.lua
+++ media/lua/client/RA_PostDeathMenu.lua
@@ -8,49 +8,45 @@
 
 require "ISUI/ISPostDeathUI"
 
-
 local og_ISPostDeathUICreateChildren = ISPostDeathUI.createChildren
 
 function ISPostDeathUI:createChildren()
     -- TODO Ugly, just for test, find a better way to add a button before PostDeath_Respawn
-	local buttonWid = 250
-	local buttonHgt = 40
-	local buttonGapY = 12
-	local buttonX = 0
-	local buttonY = 0       -- TODO Fix this, too low, but if it gets to - something then it just breaks collisionssd
-	local totalHgt = (buttonHgt * 3) + (buttonGapY * 2)
+    local buttonWid = 250
+    local buttonHgt = 40
+    local buttonGapY = 12
+    local buttonX = 0
+    local buttonY = 0       -- TODO Fix this, too low, but if it gets to - something then it just breaks collisionssd
+    local totalHgt = (buttonHgt * 3) + (buttonGapY * 2)
 
-	self:setWidth(buttonWid)
-	self:setHeight(totalHgt)
-	-- must set these after setWidth/setHeight or getKeepOnScreen will mess them up
-	self:setX(self.screenX + (self.screenWidth - buttonWid) / 2)
-	self:setY(self.screenY + (self.screenHeight - 40 - totalHgt))
+    self:setWidth(buttonWid)
+    self:setHeight(totalHgt)
+    -- must set these after setWidth/setHeight or getKeepOnScreen will mess them up
+    self:setX(self.screenX + (self.screenWidth - buttonWid) / 2)
+    self:setY(self.screenY + (self.screenHeight - 40 - totalHgt))
 
+    local button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_RespawnAsZed"), self, self.onRespawnAsZed)
+    self:configButton(button)
+    self:addChild(button)
+    self.buttonRespawn = button
+    buttonY = buttonY + buttonHgt + buttonGapY
 
-    
-	local button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_RespawnAsZed"), self, self.onRespawnAsZed)
-	self:configButton(button)
-	self:addChild(button)
-	self.buttonRespawn = button
-	buttonY = buttonY + buttonHgt + buttonGapY
-    
-	local button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_Respawn"), self, self.onRespawn)
-	self:configButton(button)
-	self:addChild(button)
-	self.buttonRespawn = button
-	buttonY = buttonY + buttonHgt + buttonGapY
+    local button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_Respawn"), self, self.onRespawn)
+    self:configButton(button)
+    self:addChild(button)
+    self.buttonRespawn = button
+    buttonY = buttonY + buttonHgt + buttonGapY
 
-	button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_Exit"), self, self.onExit)
-	self:configButton(button)
-	self:addChild(button)
-	self.buttonExit = button
-	buttonY = buttonY + buttonHgt + buttonGapY
+    button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_Exit"), self, self.onExit)
+    self:configButton(button)
+    self:addChild(button)
+    self.buttonExit = button
+    buttonY = buttonY + buttonHgt + buttonGapY
 
-	button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_Quit"), self, self.onQuitToDesktop)
-	self:configButton(button)
-	self:addChild(button)
-	self.buttonQuit = button
-
+    button = ISButton:new(buttonX, buttonY, buttonWid, buttonHgt, getText("IGUI_PostDeath_Quit"), self, self.onQuitToDesktop)
+    self:configButton(button)
+    self:addChild(button)
+    self.buttonQuit = button
 
 
 end
@@ -59,54 +55,65 @@ end
 function ISPostDeathUI:onRespawnAsZed()
     print("Respawning as zombie")
 
-	-- TODO Reapply correct appearances
-	-- TODO Reapply inventory
-	-- TODO Reapply traits
-	-- TODO Delete player zombie NPC
+    -- TODO Reapply correct appearances
+    -- TODO Reapply inventory
+    -- TODO Reapply traits
+    -- TODO Delete player zombie NPC
 
-	local player = getPlayer()
-	local player_cell = player:getCell()
-
-	getWorld():setLuaSpawnCellX(player_cell:getWorldX())
-	getWorld():setLuaSpawnCellY(player_cell:getWorldY())
-	getWorld():setLuaPosX(player:getX())
-	getWorld():setLuaPosY(player:getY())
-	getWorld():setLuaPosZ(player:getZ())
-
-	MainScreen.instance.avatar = nil
+    local old_player = getPlayer()
+    local old_player_cell = old_player:getCell()
+    local old_player_visual = old_player:getHumanVisual()
 
 
-	local forename = "Test"--getOldPlayerName()
-	local surname =  "Test" --getOldPlayerSurname()
+
+    -- Respawn in the same position
+    getWorld():setLuaSpawnCellX(old_player_cell:getWorldX())
+    getWorld():setLuaSpawnCellY(old_player_cell:getWorldY())
+    getWorld():setLuaPosX(old_player:getX())
+    getWorld():setLuaPosY(old_player:getY())
+    getWorld():setLuaPosZ(old_player:getZ())
+
+    MainScreen.instance.avatar = nil
+
+    local forename = "Test"--getOldPlayerName()
+    local surname = "Test" --getOldPlayerSurname()
 
 
-	-- MainScreen.instance.desc:setForename(forename)
-	-- MainScreen.instance.desc:setSurname(surname)
+    -- MainScreen.instance.desc:setForename(forename)
+    -- MainScreen.instance.desc:setSurname(surname)
 
-	if ISPostDeathUI.instance[self.playerIndex] then
-		ISPostDeathUI.instance[self.playerIndex]:removeFromUIManager()
-		ISPostDeathUI.instance[self.playerIndex] = nil
-	end
+    if ISPostDeathUI.instance[self.playerIndex] then
+        ISPostDeathUI.instance[self.playerIndex]:removeFromUIManager()
+        ISPostDeathUI.instance[self.playerIndex] = nil
+    end
 
-	if not self.joypadData then
-		setPlayerMouse(nil)
-		return
-	end
+    -- Reapplies the visuals
+    local new_player_visual = getPlayer():getHumanVisual()
+    new_player_visual:clear()
+    new_player_visual:copyFrom(old_player_visual)
 
-	local controller = self.joypadData.controller
-	local joypadData = JoypadState.joypads[self.playerIndex+1]
-	JoypadState.players[self.playerIndex+1] = joypadData
-	joypadData.player = self.playerIndex
-	joypadData:setController(controller)
-	joypadData:setActive(true)
-	local username = nil
-	if isClient() and self.playerIndex > 0 then
-		username = CoopUserName.instance:getUserName()
-	end
-	setPlayerJoypad(self.playerIndex, self.joypadIndex, nil, username)
 
-	self.joypadData.focus = nil
-	self.joypadData.lastfocus = nil
-	self.joypadData.prevfocus = nil
-	self.joypadData.prevprevfocus = nil
+
+
+    if not self.joypadData then
+        setPlayerMouse(nil)
+        return
+    end
+
+    local controller = self.joypadData.controller
+    local joypadData = JoypadState.joypads[self.playerIndex + 1]
+    JoypadState.players[self.playerIndex + 1] = joypadData
+    joypadData.player = self.playerIndex
+    joypadData:setController(controller)
+    joypadData:setActive(true)
+    local username = nil
+    if isClient() and self.playerIndex > 0 then
+        username = CoopUserName.instance:getUserName()
+    end
+    setPlayerJoypad(self.playerIndex, self.joypadIndex, nil, username)
+
+    self.joypadData.focus = nil
+    self.joypadData.lastfocus = nil
+    self.joypadData.prevfocus = nil
+    self.joypadData.prevprevfocus = nil
 end
